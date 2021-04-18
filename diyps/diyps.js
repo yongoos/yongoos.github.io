f38e910b2120a203b85f01d88dc83cbc4dbd4875
yongoos <ying.gaowow@gmail.com>
2021-04-18 01:59:25 -0700

    info

--- diyps/diyps.js
+++ diyps/diyps.js
@@ -1,4 +1,10 @@
 var img;
+var img2;
+var img3;
+var img4;
+var img5;
+var img6;
+var img7;
 var initials ='yg'; // your initials
 var choice = '1'; // starting choice, so it is not empty
 var screenbg = 250; // off white background
@@ -8,11 +14,18 @@ function preload() {
 // preload() runs once, it may make you wait
 //  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
 // you can link to an image on your github account
-  img = loadImage('https://dma-git.github.io/images/74.png');
+  img = loadImage('https://yongoos.github.io/images/flower1.png');
+  img2 = loadImage('https://yongoos.github.io/images/flower2.png');
+  img3 = loadImage('https://yongoos.github.io/images/flower3.png');
+  img4 = loadImage('https://yongoos.github.io/images/flower4.png');
+  img5 = loadImage('https://yongoos.github.io/images/flower5.png');
+  img6 = loadImage('https://yongoos.github.io/images/splash.png');
+  img7 = loadImage('https://yongoos.github.io/images/leaf.png');
+  img8 = loadImage('https://yongoos.github.io/images/flower8.png');
 }
 
 function setup() {
-createCanvas(600, 400);  // canvas size
+createCanvas(600, 600);  // canvas size
 background(screenbg);   // use our background screen color
 
 }
@@ -33,55 +46,57 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
   // graphic function
 
  if (toolChoice == '1' ) {  // first tool
-   
-    stroke(01);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    ellipse(mouseX, mouseY,10*(pmouseX-mouseX), 10*(pmouseY-mouseY));
+
+    image(img, mouseX-175, mouseY-175, 359, 350);
     
   } else if (toolChoice == '2') { // second tool};
+  
+    image(img2, mouseX-75, mouseY-75, 150, 150);
 
-    stroke(20);
-    line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '3') { // third tool
-
-    stroke(300, 100, 0, 80);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  
+    image(img3, mouseX-30, mouseY-30, 60, 60);
+    
   } else if (toolChoice == '4') {
 
-    stroke(0, 0, 255);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    image(img4, mouseX-15, mouseY-15, 30, 30);
+    
   } else if (key == '5') { // this tool calls a function
-    stroke(0, 0, 255);
-    testbox(20, 20, 200);
-    testbox(200, 20, 20);
-    
-    
-    
+  
+    image(img5, mouseX-125, mouseY-125, 250, 250);
+
+ 
     
     // make testbox do something!
  //   line(mouseX, mouseY, pmouseX, pmouseY);
   } else if (toolChoice == '6') {
 
-    stroke(200);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+   image(img6, mouseX-200, mouseY-200, 400, 400);
+   
   } else if (toolChoice == '7') {
+         
 
-    fill(100, 200, 100);
-    rect(mouseX, mouseY, 20, 20);
+    image(img7, mouseX* random(3), mouseY* random(3), 75, 100);
+ 
+    
   } else if (toolChoice == '8') {
 
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 20, 20);
+   image(img8, mouseX-60, mouseY-60, 120, 120);
+   
   } else if (toolChoice == '9') {
-
-    fill(300, 100, 0, 80);
-    rect(mouseX, mouseY, 40, 40);
+    
+    noStroke();
+    fill(10, 90, 60, random(255), random(255) );
+    ellipse(mouseX,mouseY, random(300) ,random(300));
+    
   } else if (toolChoice == '0') {
-    stroke(0, 0);
-    fill(random(255), random(255), random(255), random(255));
-    rect(mouseX, mouseY, 200, 150);
+    noStroke();
+    fill(10, 120, 60, random(100), random(255) );
+    ellipse(mouseX,mouseY, random(50) ,random(50));
+    
+    
   } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX, mouseY);
+    image(img, mouseX, mouseY, 100, 300);
     
   }
  }
